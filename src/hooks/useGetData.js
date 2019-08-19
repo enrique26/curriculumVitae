430812dafd27cf78d4cf0fe2cb1939186556263f
enrique26 <enriqueibilis@gmail.com>
2019-08-19 11:46:32 -0500

    build json

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,8 +1,8 @@
 import {useState,useEffect} from 'react';
 
-const API =
+const API = "https://raw.githubusercontent.com/enrique26/curriculumVitae/dev/assets/content.json";
 // "https://raw.githubusercontent.com/enrique26/curriculumVitae/2089b312abea89d06ad10c00a6e58f58014a88e0/assets/content.json";
-"https://raw.githubusercontent.com/enrique26/curriculumVitae/dev/assets/content.json";
+
 
 
 
