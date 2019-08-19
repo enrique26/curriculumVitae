2089b312abea89d06ad10c00a6e58f58014a88e0
enrique26 <enriqueibilis@gmail.com>
2019-08-19 11:10:32 -0500

    update json

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,6 +1,7 @@
 import {useState,useEffect} from 'react';
 
-const API = "https://raw.githubusercontent.com/enrique26/curriculumVitae/8162143ee7ca5661bc95d120f41dee9a709bfcb9/assets/content.json";
+const API = "https://github.com/enrique26/curriculumVitae/blob/dev/assets/content.json"
+// "https://raw.githubusercontent.com/enrique26/curriculumVitae/8162143ee7ca5661bc95d120f41dee9a709bfcb9/assets/content.json";
 
 
 const useGetData = () => {
